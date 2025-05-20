2c958ba115b96c0d6450cde8c504b01d47d88a49
Ivan_Karagachev <ivan.karagachev@gmail.com>
2025-05-20 18:38:39 +0300

    main

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://44.227.217.144:8080',
+        // target: 'http://localhost:8080',
+        target: 'http://itconnect.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
